20e3d180f4460cf9e19d501752d6265561062fd5
Rémy Hildenbrand <95428024+calimere@users.noreply.github.com>
2024-10-11 14:25:56 +0200

    Ajout des abo, des remises et des produits remisables

--- dev_framework/Message/Model/DatabaseMessage.cs
+++ dev_framework/Message/Model/DatabaseMessage.cs
@@ -5,6 +5,23 @@
         public int Count { get; set; }
         public EnumDataBaseMessage EnumDataBaseMessage { get; }
         public DatabaseMessage(EnumDataBaseMessage enumDataBaseMessage) { EnumDataBaseMessage = enumDataBaseMessage; }
+        public DatabaseMessage() { }
+
+        /// <summary>
+        /// Constructeur pour les messages de la base de données, récupérant l'objet de retour précédent mais en changeant enumDataBaseMessage et en ajoutant une exception
+        /// </summary>
+        /// <param name="databaseMessage"></param>
+        /// <param name="enumDataBaseMessage"></param>
+        /// <param name="ex"></param>
+        public DatabaseMessage(DatabaseMessage databaseMessage, EnumDataBaseMessage enumDataBaseMessage, Exception ex)
+        {
+            ReturnValue = databaseMessage.ReturnValue;
+            Count = databaseMessage.Count;
+
+            EnumDataBaseMessage = enumDataBaseMessage;
+            Exception = ex;
+        }
+
     }
 
     public class IOMessage : Message
