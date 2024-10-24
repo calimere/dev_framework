351357266121933a2cb3c00133f88f0920c258bd
Rémy Hildenbrand <95428024+calimere@users.noreply.github.com>
2024-10-24 11:20:47 +0200

    ajout des DLL de génération des PDF ajout des taux de tva par produit

---
+++ dev_framework/Manager/FileManager.cs
@@ -0,0 +1,74 @@
+﻿using DinkToPdf;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_framework.Manager
+{
+    public class FileManager
+    {
+        private SynchronizedConverter _converter;
+        private readonly SerilogManager _serilogManager;
+
+        public FileManager(SynchronizedConverter converter, SerilogManager serilogManager)
+        {
+            _converter = converter;
+            _serilogManager = serilogManager;
+        }
+
+        public bool GeneratePDF(string url, string filePath, string footer)
+        {
+            var responseData = string.Empty;
+            try
+            {
+                WebRequest request = WebRequest.Create(url);
+                using (var response = request.GetResponse())
+                {
+                    using (StreamReader responseReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        try
+                        {
+                            responseData = responseReader.ReadToEnd();
+                            var doc = new HtmlToPdfDocument()
+                            {
+                                GlobalSettings = {
+                                    ColorMode = DinkToPdf.ColorMode.Color,
+                                    Orientation = DinkToPdf.Orientation.Portrait,
+                                    PaperSize = DinkToPdf.PaperKind.A4,
+                                    Margins = new MarginSettings() { Top = 10 },
+                                    Out = filePath,
+                                    Collate = false,
+                                    PageOffset = 0,
+                                },
+                                Objects = { new ObjectSettings() {
+                                HtmlContent = responseData,
+                                WebSettings = { DefaultEncoding = "utf-8", LoadImages=true },
+                                FooterSettings = new FooterSettings
+                                {
+                                    Center="[page]/[topage]",
+                                    FontSize = 8,
+                                    Right = footer
+                                }
+                            } }
+                            };
+
+                            _converter.Convert(doc);
+                            return true;
+                        }
+                        catch (Exception ex)
+                        {
+                            _serilogManager.Error("Erreur durant la génération du pdf", ex);
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch { return false; }
+        }
+    }
+}
---
+++ dev_framework/Manager/Model/FileResultModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dev_framework.Manager.Model
+{
+    public class FileResultModel
+    {
+        public string MimeType { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
