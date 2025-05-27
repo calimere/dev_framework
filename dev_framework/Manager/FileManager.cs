f88bd42d2f54a6cbd70c1d08a3e7e09e3e5bb449
Rémy Hildenbrand <95428024+calimere@users.noreply.github.com>
2025-05-27 20:42:22 +0200

    correction pour la mise en prod

--- dev_framework/Manager/FileManager.cs
+++ dev_framework/Manager/FileManager.cs
@@ -1,5 +1,6 @@
 ﻿using dev_framework.Message.Model;
 using DinkToPdf;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Drawing.Imaging;
@@ -70,6 +71,48 @@ namespace dev_framework.Manager
                 }
             }
             catch (Exception ex) { return new IOMessage(EIOMessage.Error) { Exception = ex }; }
+        }
+
+        public async Task<IOMessage> GeneratePDFFromPython(string pythonServer, string url, string filePath)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var json = new { url = url, filePath = filePath };
+                    var content = new StringContent(JsonConvert.SerializeObject(json), Encoding.UTF8, "application/json");
+                    var response = await httpClient.PostAsync(pythonServer, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Lire le contenu PDF (stream)
+                        byte[] pdfBytes = await response.Content.ReadAsByteArrayAsync();
+
+                        // Enregistrer dans un fichier
+                        await System.IO.File.WriteAllBytesAsync("output.pdf", pdfBytes);
+
+                        Console.WriteLine("PDF téléchargé et sauvegardé en output.pdf");
+                    }
+                    else
+                    {
+                        string error = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine($"Erreur : {response.StatusCode}");
+                        Console.WriteLine(error);
+                    }
+                }
+
+                return new IOMessage(EIOMessage.Success) { ReturnValue = new { FilePath = filePath } };
+            }
+            catch (HttpRequestException ex)
+            {
+                _serilogManager.Error("Erreur de requête HTTP lors de la génération du PDF", ex);
+                return new IOMessage(EIOMessage.Error) { Exception = ex };
+            }
+            catch (Exception ex)
+            {
+                _serilogManager.Error("Erreur générale lors de la génération du PDF", ex);
+                return new IOMessage(EIOMessage.Error) { Exception = ex };
+            }
         }
     }
 }
